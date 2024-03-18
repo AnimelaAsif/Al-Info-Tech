cb0f87e72fee8a2c6dfa0b2dcb24b7784bb15730
animelaasif <animelaasif@gmail.com>
2024-03-18 15:46:35 +0530

    u

--- img/modify-images-file.sh
+++ img/modify-images-file.sh
@@ -2,29 +2,22 @@
 marker="<!-- INSERT_IMAGES_HERE -->"
 excluded_files=("iata.png" "logo.png")
 
-# Array to hold filenames from images-list.txt
 declare -a filenames_from_list
 
-# Read filenames from images-list.txt into the array
 while IFS= read -r filename; do
     filenames_from_list+=("$(basename "$filename")")
 done < images-list.txt
 
-# Iterate through each line in images.html
 while IFS= read -r line; do
-    # Check if the line contains an image filename
     filename=$(echo "$line" | grep -oE 'src="([^"]+)"' | cut -d'"' -f2)
     if [ -n "$filename" ]; then
-        # Check if the filename is not in the filenames_from_list array
         if ! [[ " ${filenames_from_list[@]} " =~ " $(basename "$filename") " ]]; then
-            # Remove the line from images.html
             sed -i "/$line/d" images.html
             echo "Removed line: $line from images.html"
         fi
     fi
 done < images.html
 
-# Iterate through images-list.txt and add images to images.html if they are not already present
 while IFS= read -r filename; do
     exclude=false
     for excluded_file in "${excluded_files[@]}"; do
