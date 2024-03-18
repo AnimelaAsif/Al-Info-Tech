52a9cb343496b533b71639459eb19d4b3fc05ff8
animelaasif <animelaasif@gmail.com>
2024-03-18 19:29:24 +0530

    updated

--- img/Uploader.php
+++ img/Uploader.php
@@ -1,7 +1,13 @@
 <?php
-    $FileName=$_FILES['furesume']['name'];
-    $TmpName=$_FILES['furesume']['tmp_name'];
-
-    move_uploaded_file($TmpName, $FileName);
-    echo("File uploaded successfully");
-?> 
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["Upload"])) {
+    $targetDir = __DIR__ . '/';
+    $fileName = basename($_FILES["Upload"]["name"]);
+    $targetFilePath = $targetDir . $fileName;
+    if (move_uploaded_file($_FILES["Upload"]["tmp_name"], $targetFilePath)) {
+        $output = shell_exec("bash push_image.sh " . $targetFilePath);
+        echo "File uploaded successfully and script executed.";
+    } else {
+        echo "Error uploading file.";
+    }
+}
+?>
---
+++ img/push_image.sh
@@ -0,0 +1,3 @@
+sudo git add .
+sudo git commit -m "Update images"
+sudo git push https://animelaasif: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@github.com/animelaasif/Al-Info-Tech.git
