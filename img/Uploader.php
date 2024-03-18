b92534563507549d355d91b394169b62049f26bf
animelaasif <animelaasif@gmail.com>
2024-03-18 20:46:06 +0530

    updated

--- img/Uploader.php
+++ img/Uploader.php
@@ -1,19 +1,13 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["Upload"])) {
-    // Directory of the current PHP script
     $targetDir = dirname(__FILE__) . '/';
-    // File name
     $fileName = basename($_FILES["Upload"]["name"]);
-    // File path
     $targetFilePath = $targetDir . $fileName;
 
-    // Upload file to server
     if (move_uploaded_file($_FILES["Upload"]["tmp_name"], $targetFilePath)) {
         shell_exec("git add $fileName");
         shell_exec("git commit -m 'Uploaded $fileName'");
         shell_exec("git push https://animelaasif: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@github.com/animelaasif/Al-Info-Tech.git");
-        shell_exec("sh image-list.sh");
-        shell_exec("sh modify-images-files.sh");
         echo "File uploaded successfully.";
     } else {
         echo "Error uploading file.";
