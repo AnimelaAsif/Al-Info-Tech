3bfbd58e05640d33a215eed5673204fa434b94bc
Apache <apache@ip-172-31-2-0.ap-south-1.compute.internal>
2024-03-19 07:02:45 +0000

    Uploaded SSC.jpg

--- img/Uploader.php
+++ img/Uploader.php
@@ -5,7 +5,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["Upload"])) {
     $targetFilePath = $targetDir . $fileName;
 
     if (move_uploaded_file($_FILES["Upload"]["tmp_name"], $targetFilePath)) {
-        shell_exec("git add $fileName");
+        $output1 = shell_exec("sh image-list.sh 2>&1");
+        $output2 = shell_exec("sh modify-images-file.sh 2>&1");
+        shell_exec("git add .");
         shell_exec("git commit -m 'Uploaded $fileName'");
         shell_exec("git push https://animelaasif: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@github.com/animelaasif/Al-Info-Tech.git");
         echo "File uploaded successfully.";
